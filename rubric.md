b224387c017f2df1d48706fb8aced01b7e910f74
Andy <andrew.sunglae.kim@gmail.com>
2016-04-06 10:40:45 -0400

    adds comments and rubric

---
+++ rubric.md
@@ -0,0 +1,15 @@
+# Rubric
+
+## Project Workflow
+
+Performing - User stories/wireframes look great. Looks like you got pretty close to the landing page you planned!
+
+## Technical Requirement
+
+Performing - App is functional. You're really close to fixing the same click bug. This project shows full understanding of course content covered in the first 3 weeks of the course. Great work! While reflecting on this project or working on it further, you should look into how it might benefit with an object oriented approach.
+
+## Code Quality
+Performing - Great clean code. I would look for opportunities to abstract functionality into functions. In this way, your code becomes more readable and manageable. Great comments throughout and well indented. I really like that you abstracted functionality into separate files.
+
+## Creativity/Interface
+Excelling - The styles are very simple and clean. Their are subtleties throughout the app that makes it really stand out. Background greys changing when you select a match, showing all matches before the game starts, its the little things that make interface really shine. Great work!
--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ document.querySelector("button").addEventListener("click", init)
 
 function init (){
   document.body.removeChild(document.querySelector("div.welcome"));
+  // this is a nice touch
   setTimeout (function(){for(var i=0; i<allDivs.length; i++){
     allDivs = document.querySelectorAll("div.box")
     allDivs[i].classList.add('cover')
@@ -23,6 +24,9 @@ for(var i=0; i<allDivs.length; i++){
     this.classList.add('clicked')
     this.classList.remove('cover');
     lastClicked.unshift(this);
+    // in the way your code is written, do you want the click counter to increment no matter what?
+    // if they click the same element, the click counter will still go up, put this incremementation in the if conditionals below
+    // once you figure this part out, i think you'll have the solution for the click on the same div bug.
     clickCounter++;
 
     if (clickCounter === 1 && this.classList.includes !== "clicked"){
@@ -34,6 +38,10 @@ for(var i=0; i<allDivs.length; i++){
       console.log(guess2);
       //reset counter, hide things that didn't match
     }
+
+    // this if conditional seems complicated
+    // why not instead nest this logic inside of the above conditionals?
+    // would also be good to abstract this into a function, something like confirmMatch
     if(guess1 && guess2 && guess1 === guess2 && clickCounter === 2){
       console.log("it's a match");
       clickCounter = 0;
@@ -42,6 +50,7 @@ for(var i=0; i<allDivs.length; i++){
       shufflePieces(bgDivs);
 
     }
+    // this would also be a good place to abstract, hideCards or something like that
     else if (guess1 && guess2 && guess1 !== guess2 && clickCounter === 2){
       setTimeout(function(){
         lastClicked[0].classList.add('cover');
